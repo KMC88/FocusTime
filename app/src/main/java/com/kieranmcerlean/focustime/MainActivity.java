d055267d0b417e9d0c337dc74ab5eac2ffb872ca
Kieran McErlean <kmcerlean03@qub.ac.uk>
2019-01-07 16:25:56 +0000

    MainActivity.java amended. Start, Pause, Reset button implementation added for basic countdown timer.

--- app/src/main/java/com/kieranmcerlean/focustime/MainActivity.java
+++ app/src/main/java/com/kieranmcerlean/focustime/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final long START_TIME_MILLISECONDS = 6000000;
+    public static final long START_TIME_MILLISECONDS = 1500000;
 
     private TextView mTVCountdown;
     private Button mButtonStart;
@@ -56,18 +58,50 @@ public class MainActivity extends AppCompatActivity {
                 resetTimer();
             }
         });
+
+        updateTimerText();
     }
 
     private void startTimer(){
+        mCountDownTimer = new CountDownTimer(mTimeRemaining, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeRemaining = millisUntilFinished;
+                updateTimerText();
+            }
 
+            @Override
+            public void onFinish() {
+                mTimerIsRunning = false;
+                mButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+
+        mTimerIsRunning = true;
+        mButtonPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
 
     }
 
     private void pauseTimer(){
-
+        mCountDownTimer.cancel();
+        mTimerIsRunning = false;
+        mButtonStart.setText("start");
+        mButtonReset.setVisibility(View.VISIBLE);
     }
 
     private void resetTimer(){
+        mTimeRemaining = START_TIME_MILLISECONDS;
+        updateTimerText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
 
+    private void updateTimerText(){
+        int minutes = (int) (mTimeRemaining/1000)/60;
+        int seconds = (int) (mTimeRemaining/1000)%60;
+
+        String timeRemainingFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+
+        mTVCountdown.setText(timeRemainingFormatted);
     }
 }
