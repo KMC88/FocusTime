c12acfb91c59f4cc4218f1f2f407cb12ad2d4b88
Kieran McErlean <33010651+KMC88@users.noreply.github.com>
2019-01-25 17:22:06 +0000

    Changes made to mainActivity.java. Ability to change countdown timer begun

--- app/src/main/java/com/kieranmcerlean/focustime/MainActivity.java
+++ app/src/main/java/com/kieranmcerlean/focustime/MainActivity.java
@@ -11,33 +11,43 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Initial time in milliseconds
+     */
     public static final long START_TIME_MILLISECONDS = 1500000;
+    private long timeInMills;
 
+    //instance vars for TextViews and Buttons defined in activity_main.xml
     private TextView mTVCountdown;
     private Button mButtonStart;
     private Button mButtonPause;
     private Button mButtonReset;
+    private Button mButtonSetTime;
 
     private CountDownTimer mCountDownTimer;
     private boolean mTimerIsRunning;
 
     private long mTimeRemaining = START_TIME_MILLISECONDS;
 
-
+    /**
+     * Inflates the main layout and widgets in the main layout
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTVCountdown = (TextView)findViewById(R.id.tv_counter);
-        mButtonStart = (Button)findViewById(R.id.button_start);
-        mButtonPause = (Button)findViewById(R.id.button_pause);
-        mButtonReset = (Button)findViewById(R.id.button_reset);
+        mTVCountdown = (TextView) findViewById(R.id.tv_counter);
+        mButtonStart = (Button) findViewById(R.id.button_start);
+        mButtonPause = (Button) findViewById(R.id.button_pause);
+        mButtonReset = (Button) findViewById(R.id.button_reset);
+        mButtonSetTime = (Button) findViewById(R.id.button_set_time);
 
         mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!mTimerIsRunning){
+                if (!mTimerIsRunning) {
                     startTimer();
                 }
             }
@@ -46,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         mButtonPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mTimerIsRunning){
+                if (mTimerIsRunning) {
                     pauseTimer();
                 }
             }
@@ -58,10 +68,20 @@ public class MainActivity extends AppCompatActivity {
                 resetTimer();
             }
         });
-
         updateTimerText();
+
+        mButtonSetTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO: implement ability for user to change counter time
+            }
+        });
     }
 
+    /**
+     * Starts the countdown timer initially or after being paused or reset
+     * Sets the boolean mTimerIsRunning to true and the mResetButton visibility to Invisible
+     */
     private void startTimer(){
         mCountDownTimer = new CountDownTimer(mTimeRemaining, 1000) {
             @Override
@@ -82,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
         mButtonReset.setVisibility(View.INVISIBLE);
 
     }
+
 
     private void pauseTimer(){
         mCountDownTimer.cancel();
@@ -104,4 +125,14 @@ public class MainActivity extends AppCompatActivity {
 
         mTVCountdown.setText(timeRemainingFormatted);
     }
+
+    public long getTimeInMills() {
+        return timeInMills;
+    }
+
+    public void setTimeInMills(long timeInMills) {
+        this.timeInMills = timeInMills;
+    }
+
+
 }
